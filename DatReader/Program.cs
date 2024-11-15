806c650eb615f4d144e239fa5239382e28da0d63
ctricks <mon.gerald28@gmail.com>
2024-11-15 10:27:03 +0800

    Initial Source Code

---
+++ DatReader/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DatReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("*                                                                     *");
+            Console.WriteLine("*              Welcome to DatFile Reader/Extractor                    *");
+            Console.WriteLine("*                                                                     *");
+            Console.WriteLine("***********************************************************************");
+
+            string DirectoryPath = "C:\\DatFile";
+
+            Console.WriteLine("\n\n\n");
+
+            if (!Directory.Exists(DirectoryPath))
+            {
+                Console.WriteLine("Error: Cannot continue. Please create folder first in this path: " + DirectoryPath);
+                Console.WriteLine("\n\nPress any key to terminate...");
+
+
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+
+            Console.WriteLine("Instruction : Put your .dat file in this directory "+ DirectoryPath + "\n\nPress Ctrl + C to terminate....\n\n");
+            Console.WriteLine("\n\n");
+
+            
+
+            int counter = 5;
+            
+
+            clsFunction MyFunction = new clsFunction();
+
+            do
+            {
+                Console.Write("\rCheck file...." + counter);
+                Console.Write("\r");
+                
+
+                counter--;
+
+                Thread.Sleep(1000);
+
+                
+                if (counter == 0)
+                {
+                    Console.Write("\rCheck file...." + counter);
+                    Thread.Sleep(1000);
+
+                    if(MyFunction.CheckFile(DirectoryPath))
+                    {
+                        int filecount = 0;
+                        string FileNameToProcess = MyFunction.GetFileName(DirectoryPath);
+
+                        if (FileNameToProcess == null)
+                        {
+                            counter = 5;
+                            Console.SetCursorPosition(0, 17);
+                            continue;
+                        }
+
+                        filecount = MyFunction.GetFileDat(DirectoryPath);
+                        string DisplayUI = string.Empty;
+
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tFile Found: " + filecount;
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tFilename: " + FileNameToProcess;
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tReading File....";
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tFile Content....\n";
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                        DisplayUI = MyFunction.ReadDatFile(FileNameToProcess);
+                        MyFunction.ExtractMyDate(DisplayUI, DirectoryPath);
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tDone Reading....";
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);                        
+                        MyFunction.PlacedDone(FileNameToProcess);
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tPlaced Done....";
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                        Console.WriteLine(DisplayUI);                        
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tPut it on DONE....";
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                        DisplayUI = DateTime.Now.ToString("yyyy-dd-mm hh:mm:ss") + "\tDone Processing....\n\n";
+                        Console.WriteLine(DisplayUI);
+                        MyFunction.LogActivity(FileNameToProcess, DisplayUI);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(0, 17);
+                    }
+
+                    counter = 5;
+                }
+                    
+            } while (1 == 1);
+
+
+        }
+    }
+}
---
+++ DatReader/clsFunction.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DatReader
+{
+    internal class clsFunction
+    {
+        public bool CheckFile(string PathFile) {
+            if (Directory.GetFiles(PathFile).Length > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int GetFileDat(string PathFile)
+        {
+            return Directory.GetFiles(PathFile,"*.dat").Length;
+        }
+        public string GetFileName(string PathFile)
+        {
+            if (Directory.GetFiles(PathFile, "*.dat").Length > 0)
+            {
+                return Directory.GetFiles(PathFile, "*.dat")[0].ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public string ReadDatFile(string FileToProcess)
+        {
+            return File.ReadAllText(FileToProcess);
+        }
+        public void PlacedDone(string FileToProcess)
+        { 
+            FileInfo FileInfo = new FileInfo(FileToProcess);
+
+            DirectoryInfo di = new DirectoryInfo(FileToProcess);
+            string DonePath = FileToProcess.Replace(di.Name, "");
+
+            if (!Directory.Exists(DonePath + "Done\\"))
+                Directory.CreateDirectory(DonePath+ "Done\\");
+
+            string DoneDirectory = DonePath + "Done\\" + FileInfo.Name;
+
+            if(File.Exists(DoneDirectory))
+                File.Delete(DoneDirectory);
+
+            File.Move(FileToProcess, DoneDirectory);
+
+        }
+        public void LogActivity(string FileToProcess,string ActivityName)
+        {
+            DirectoryInfo di = new DirectoryInfo(FileToProcess);
+
+            if(!Directory.Exists(FileToProcess.Replace(di.Name, "") + "\\Logs\\"))
+            {
+                Directory.CreateDirectory(FileToProcess.Replace(di.Name, "") + "\\Logs\\");
+            }
+
+            string LogPath = FileToProcess.Replace(di.Name,"") + "\\Logs\\" + DateTime.Now.ToString("MMddyyyy") + ".log";
+            File.AppendAllText(LogPath,"\n" + ActivityName);
+        }
+        public void ExtractMyDate(string DataContents,string FilePath)
+        {
+            string PatternText = string.Empty;
+            string PatternFile = FilePath + "\\Pattern\\Pattern.txt";
+
+            if (File.Exists(PatternFile))
+            {
+                PatternText = File.ReadAllText(PatternFile);
+
+                if (PatternFile.Length > 0)
+                {
+
+                    string ExtractContent = Regex.Match(DataContents, PatternText, RegexOptions.Multiline).Value.ToString();
+                    if (!Directory.Exists(FilePath + "\\Extract"))
+                    {
+                        Directory.CreateDirectory(FilePath + "\\Extract");
+                    }
+
+                    if (ExtractContent.Length > 0)
+                    {
+                        string SavePath = FilePath + "\\Extract\\Extract_" + DateTime.Now.ToString("MMddyyyy") + ".txt";
+
+                        if (File.Exists(SavePath))
+                            File.Delete(SavePath);
+
+                        File.AppendAllText(SavePath, "\n" + ExtractContent);
+                    }
+                }else
+                {
+                    string SavePath = FilePath + "\\Extract\\Extract_" + DateTime.Now.ToString("MMddyyyy") + ".txt";
+                    
+                    if (File.Exists(SavePath))
+                        File.Delete(SavePath);
+
+                    File.AppendAllText(SavePath, "\n" + DataContents);
+                }
+            }
+        }
+    }
+}
